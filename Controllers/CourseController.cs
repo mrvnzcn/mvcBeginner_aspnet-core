512d458788f8230d55c8e8c60e0af46202d970eb
mrvnzcn <imervanozcan@gmail.com>
2024-01-27 15:45:38 +0300

    models

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using basics.Models;
 
 namespace basics.Controllers;
 
 public class CourseController: Controller {
     public IActionResult Index()
     {
-        return View();
+        var kurs = new Course();
+        kurs.Id = 1;
+        kurs.Title = "Aspnet core kursu";
+        kurs.Description = "guzel bir kurs";
+        return View(kurs);
     }
 
     public IActionResult List()
