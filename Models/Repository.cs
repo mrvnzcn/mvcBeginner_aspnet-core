84bf55e2a73cf9c6fa9f94f9974613c02a484167
mrvnzcn <imervanozcan@gmail.com>
2024-01-27 19:46:40 +0300

    Repository

---
+++ Models/Repository.cs
@@ -0,0 +1,24 @@
+namespace basics.Models
+{
+    public class Repository
+    {
+        private static readonly List<Course> _courses = new();
+        static Repository() {
+            _courses = new List<Course>()
+            {
+                new Course() {Id=1, Title="aspnet kursu", Description="guzel bir kurs", Image="1.jpg"},
+                new Course() {Id=2, Title="php kursu", Description="guzel bir kurs", Image="2.jpg"},
+                new Course() {Id=3, Title="django kursu", Description="guzel bir kurs", Image="3.jpg"},
+                new Course() {Id=4, Title="javascript kursu", Description="guzel bir kurs", Image="4.jpg"}
+            };
+        }
+        
+
+        public static List<Course> Courses
+        {
+            get {
+                return _courses;
+            }   
+        }
+    }
+}
