7ef8082aae039e936e4bea299873658af24def25
mrvnzcn <imervanozcan@gmail.com>
2024-01-27 19:30:18 +0300

    route links

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -14,6 +14,16 @@ public class CourseController: Controller {
         return View(kurs);
     }
 
+    public IActionResult Details()
+    {
+        var kurs = new Course();
+        kurs.Id = 1;
+        kurs.Title = "Aspnet core kursu";
+        kurs.Description = "guzel bir kurs";
+        kurs.Image = "1.jpg";
+        return View(kurs);
+    }
+
     public IActionResult List()
     {
         var kurslar = new List<Course>()
